733acea1eff5e35667cd8c888a21a5fe0bf30859
Samman Rahman <142563451+Samman-Rahman@users.noreply.github.com>
2023-08-19 12:26:40 +0600

    Create 5. details of chicken items ordered.sql

---
+++ 5. details of chicken items ordered.sql
@@ -0,0 +1,19 @@
+Command to find out the details of chicken items ordered
+
+SELECT * FROM `items.Items`
+
+where name like '%Chicken%'
+
+Result - 
+
+id	order_id	name	is_veg
+17	118594611361	Chicken Fried Rice + Bbq Shredded Chicken Sauce	0
+18	118594611361	Burnt Garlic Chicken Rice + Chilly Shredded Chicken (Serves 1)	0
+19	118594611361	Butter Chicken	0
+83	86796819721	Chicken Schezwan Fried Rice	0
+85	86796243798	Chicken Parcel	0
+86	86796243798	Chicken Sausage	0
+106	63528835993	Peri Peri Chicken with Egg Corn Rice	0
+149	43875533945	Chicken Fried Rice	0
+150	43875533945	Dragon Chicken	0
+171	20698577867	Crunchy Chicken	0
