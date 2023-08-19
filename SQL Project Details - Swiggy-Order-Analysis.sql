ee882ddab92d1d49148dd67d61a9e6781bc05810
Samman Rahman <142563451+Samman-Rahman@users.noreply.github.com>
2023-08-19 11:57:54 +0600

    Delete SQL Project Details - Swiggy-Order-Analysis.sql

--- SQL Project Details - Swiggy-Order-Analysis.sql
+++
@@ -1,4 +0,0 @@
-This project aims to analyse a customer data of a food delivery app named Swiggy. Throughout the project, I tried to analyse how SQL can be utilied to find meaningful insights from the data's which can be critical to drive business decisions. Please find the links to the data I used below:
-
-Items; https://drive.google.com/file/d/1tBhnXmWmzTlR9ZdT_sUcpmG5ZGKBk8OA/view
-Orders: https://drive.google.com/file/d/1W2-nBsOVEAPUhmi7jkTPhRhuah1tYznu/view
