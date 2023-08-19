22da7a377340bbca947df65cf3ffb6cecd6d79dd
Samman Rahman <142563451+Samman-Rahman@users.noreply.github.com>
2023-08-19 12:35:00 +0600

    Create 6. details of paratha items ordered.sql

---
+++ 6. details of paratha items ordered.sql
@@ -0,0 +1,13 @@
+Command to find out the details of paratha items ordered
+
+SELECT * FROM `items.Items`
+
+where name like '%Paratha%'
+
+Result -
+
+id	order_id	name	is_veg
+1	127143465064	Paratha Kuruma	1
+9	124552896092	Onion Aloo Mixed Paratha	1
+51	116666857091	Aloo Paratha With Dal Makhani	1
+95	65256281802	Mutter Paneer & Paratha Lunchbox	1
